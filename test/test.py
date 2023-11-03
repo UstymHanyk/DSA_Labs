41455872b1f722390c36f4981ff71d61f14a1517
UstymHanyk <ustym.hanyk@ukr.net>
2023-11-03 08:51:35 +0200

    reformatted test

--- test/test.py
+++ test/test.py
@@ -1,16 +1,19 @@
 from src.main import find_shortest_path
 
+
 def test_shortest_path_1():
     board_size = 8
     start_position = (7, 0)
     destination_position = (0, 7)
     assert find_shortest_path(board_size, start_position, destination_position) == 6
 
+
 def test_shortest_path_2():
     board_size = 8
     start_position = (0, 0)
     destination_position = (2, 2)
     assert find_shortest_path(board_size, start_position, destination_position) == 4
+
 
 def test_no_path():
     board_size = 1
